4a3f9a6d16238a3f8fc43c9a8aab4a9c4d5d3e3b
eoeefosa <eoeefosa@gmail.com>
2024-05-26 07:34:56 +0100

    done user auth

--- models/user.js
+++ models/user.js
@@ -1,32 +1,35 @@
 import mongoose, { Schema, Document } from "mongoose";
 
 const userSchema = new Schema(
-    {
-      username: String,
-      password: String,
-      email: String,
-      phone_no: String,
-      address: String,
-      balance: { type: Number, default: 0 },
-      plan: { type: String, default: "Lite" },
-      plan_updatedate: { type: Date },
-      plan_expiringdate: { type: Date },
-      user_role: String,
-      img_url: String,
-      img_id: String,
-      posts: [String],
-      post_count: { type: Number, default: 0 },
-      follower: [{ type: Schema.Types.ObjectId, ref: "User" }],
-      following: [{ type: Schema.Types.ObjectId, ref: "User" }],
-      points: { type: Number, default: 0 },
-      timeStamp: Number,
-      createdAt: { type: Date, default: Date.now() },
-      lastSeen: { type: Date, default: Date.now() },
-      is_online: { type: Boolean, default: true },
-      is_deleted: { type: Boolean, default: false },
-    },
-    { collection: "users" }
+  {
+    username: String,
+    password: String,
+    email: String,
+    phone_no: String,
+    address: String,
+    referralCode: { type: String, unique: true },
+    referredBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    referredUsers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    bonusPoints: { type: Number, default: 0 },
+    balance: { type: Number, default: 0 },
+    plan: { type: String, default: "Lite" },
+    plan_updatedate: { type: Date },
+    plan_expiringdate: { type: Date },
+    user_role: String,
+    img_url: String,
+    img_id: String,
+    posts: [String],
+    post_count: { type: Number, default: 0 },
+    follower: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    following: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    points: { type: Number, default: 0 },
+    timeStamp: Number,
+    createdAt: { type: Date, default: Date.now() },
+    lastSeen: { type: Date, default: Date.now() },
+    is_online: { type: Boolean, default: true },
+    is_deleted: { type: Boolean, default: false },
+  },
+  { collection: "users" }
 );
-  
 
 export default mongoose.model("Users", userSchema);
--- routes/index.js
+++ routes/index.js
@@ -3,18 +3,56 @@ import { Router } from "express";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import Subscription from "../models/Subscription.js";
+import rateLimit from "express-rate-limit";
+import { body, validationResult } from "express-validator";
+
 import User from "../models/user.js";
+import crypto from "crypto";
+
 const router = Router();
 
-// var login = require("../views/auth/login.ejs");
+function generateReferralCode() {
+  return crypto.randomBytes(4).toString("hex");
+}
+
+// Middleware to verify JWT and extract user information
+const verifyToken = (req, res, next) => {
+  const token = req.cookies.token; // Assuming token is stored in a cookie named 'token'
+
+  if (!token) {
+    return res.redirect("/login"); // Redirect to login if token is not present
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET || "my secret");
+    req.user = decoded; // Attach user information to the request object
+    next();
+  } catch (error) {
+    return res.redirect("/login"); // Redirect to login if token is invalid
+  }
+};
+
+// Rate limiter middleware to prevent brute force attacks
+const loginLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 5, // limit each IP to 5 login requests per windowMs
+  message: "Too many login attempts, please try again later.",
+});
+
+// Rate limiter middleware to prevent brute force attacks
+const registerLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 5, // limit each IP to 5 register requests per windowMs
+  message: "Too many registration attempts, please try again later.",
+});
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render('index', {
+  res.render("index", {
     messages: {
-      error: req.flash('error'),
-      success: req.flash('success')
-    }
+      error: req.flash("error"),
+      success: req.flash("success"),
+    },
   });
 });
 
@@ -47,120 +85,214 @@ router.post("/subscribe", async (req, res) => {
   }
 });
 
-router.post("/login", async function async(req, res, next) {
-  const { username, password } = req.body;
-  // Handle login logic here
-  if (!username || !password) {
-    res.render("auth/login", {
-      errorMessage: "Username and password are required",
-    });
-  }
+/**
+ * Login post
+ */
+router.post(
+  "/login",
+  loginLimiter,
+  [
+    body("username")
+      .trim()
+      .isLength({ min: 1 })
+      .withMessage("Username is required")
+      .escape(),
+    body("password")
+      .trim()
+      .isLength({ min: 1 })
+      .withMessage("Password is required")
+      .escape(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
 
-  try {
-    const user = await User.findOne({ username: username });
-    if (!user) {
+    if (!errors.isEmpty()) {
       return res.render("auth/login", {
-        errorMessage: "Incorrect username or password",
+        errorMessage: errors
+          .array()
+          .map((error) => error.msg)
+          .join(", "),
       });
     }
 
-    const correctPassword = await bcrypt.compare(password, user.password);
+    const { username, password } = req.body;
 
-    //TODO: test this code for returning true or false
-    if (!correctPassword) {
+    try {
+      const user = await User.findOne({ username });
+      if (!user) {
+        return res.render("auth/login", {
+          errorMessage: "Incorrect username or password",
+        });
+      }
+
+      const correctPassword = await bcrypt.compare(password, user.password);
+      if (!correctPassword) {
+        return res.render("auth/login", {
+          errorMessage: "Incorrect username or password",
+        });
+      }
+
+      const token = jwt.sign(
+        {
+          _id: user._id,
+          email: user.email,
+          username: user.username,
+        },
+        process.env.JWT_SECRET || "my secret",
+        { expiresIn: "7d" }
+      );
+
+      // Update user status
+      user.is_online = true;
+      await user.save();
+
+      // Set the token in a cookie or send it in the response
+      res.cookie("token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+      });
+      return res.redirect("/dashboard");
+    } catch (error) {
+      console.error("Login error:", error); // Log error details for debugging
       return res.render("auth/login", {
-        errorMessage: "Incorrect username or password",
+        errorMessage: "Server error, please try again later.",
       });
     }
-    const token = jwt.sign(
-      {
-        _id: user._id,
-        email: user.email,
-        username: user.username,
-      },
-      process.env.JWT_SECRET || "my secret",
-      { expiresIn: "7d" }
-    );
-
-    user.is_online = true;
-    await user.save();
-
-    return res.redirect("/dashboard");
-  } catch (error) {
-    res.render("auth/login", {
-      errorMessage: `server error: ${error.message}`,
-    });
   }
-});
+);
 
 router.get("/register", function (req, res, next) {
   res.render("auth/register", { errorMessage: "" });
 });
+/**
+ * Register post
+ */
+router.post(
+  "/register",
+  registerLimiter,
+  [
+    body("username")
+      .trim()
+      .isLength({ min: 1 })
+      .withMessage("Username is required")
+      .escape(),
+    body("password")
+      .trim()
+      .isLength({ min: 6 })
+      .withMessage("Password must be at least 6 characters")
+      .escape(),
+    body("email")
+      .trim()
+      .isEmail()
+      .withMessage("Invalid email")
+      .normalizeEmail(),
+    body("phone_no")
+      .trim()
+      .isLength({ min: 10 })
+      .withMessage("Phone number must be at least 10 digits")
+      .escape(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
 
-// register
-router.post("/register", async function (req, res, next) {
-  try {
-    console.log(req.body);
+    if (!errors.isEmpty()) {
+      return res.render("auth/register", {
+        errorMessage: errors
+          .array()
+          .map((error) => error.msg)
+          .join(", "),
+      });
+    }
+
     const { username, password, email, phone_no, address, referral } = req.body;
-    // chicking if the required fields are missing
-    if (!username || !password || !email || !phone_no) {
+
+    try {
+      // Check for existing account with username
+      const existingUsername = await User.findOne({ username });
+      if (existingUsername) {
+        return res.render("auth/register", {
+          errorMessage: `User with this username: ${username} already exists`,
+        });
+      }
+
+      // Generate a unique referral code
+      const userReferralCode = generateReferralCode();
+
+      const user = new User({
+        username,
+        password: await bcrypt.hash(password, 10),
+        phone_no,
+        referralCode: userReferralCode,
+        email,
+        address: address || "",
+        timeStamp: Date.now(),
+        follower: [],
+        posts: [],
+      });
+
+      if (referral) {
+        // Find the user with the referral code
+        const referringUser = await User.findOne({ referralCode: referral });
+        if (referringUser) {
+          user.referredBy = referringUser._id;
+          referringUser.referredUsers.push(user._id);
+
+          // Award bonus points to the referring user
+          referringUser.bonusPoints += 10; // Example bonus points
+
+          await referringUser.save();
+        }
+      }
+
+      const currentUser = await user.save();
+
+      const token = jwt.sign(
+        {
+          _id: currentUser._id,
+          email: currentUser.email,
+          username: currentUser.username,
+        },
+        process.env.JWT_SECRET || "my secret",
+        { expiresIn: "7d" }
+      );
+
+      // Set the token in a cookie or send it in the response
+      res.cookie("token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+      });
+      return res.redirect("/dashboard");
+    } catch (error) {
+      console.error("Registration error:", error); // Log error details for debugging
       return res.render("auth/register", {
-        errorMessage: `All required field must be filled`,
+        errorMessage: "Server error, please try again later.",
       });
     }
-
-    // Check for existing account with username
-    const existingUsername = await User.findOne({ username });
-    if (existingUsername) {
-      return res.render("auth/register", {
-        errorMessage: `User with this username: ${username} already exist`,
-      });
-    }
-
-    const user = new User();
-    user.username = username;
-    user.password = await bcrypt.hash(password, 10);
-    user.phone_no = phone_no;
-    user.email = email;
-    user.address = address || "";
-    user.timeStamp = Date.now();
-    user.follower = [];
-    user.posts = [];
-
-    const currentuser = await user.save();
-
-    const token = jwt.sign(
-      {
-        _id: currentuser._id,
-        email: currentuser.email,
-        username: currentuser.username,
-      },
-      process.env.JWT_SECRET || "my secret",
-      { expiresIn: "30000 s" }
-    );
-
-    // return responsejson(
-    //   true,
-    //   `${currentuser.username} account created successfully`,
-    //   res,
-    //   200,
-    //   token
-    // );
-    // return res.status(200).send({ status: "success", msg: "success", token });
-    return res.redirect("/dashboard");
-  } catch (error) {
-    return res.render("auth/login", {
-      errorMessage: `server error: ${error.message}`,
-    });
   }
-});
+);
 
 router.get("/forgot-password", function (req, res, next) {
   res.render("auth/forgot-password", { title: "login" });
 });
 
-router.get("/logout", function (req, res, next) {
-  res.render("auth/logout", { title: "login" });
+// Logout route
+router.get("/logout", function (req, res) {
+  // Clear the authentication token cookie
+  res.clearCookie("token", {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+  });
+
+  // Optional: If using session-based authentication, destroy the session
+  // req.session.destroy(err => {
+  //   if (err) {
+  //     return res.redirect('/dashboard'); // Handle session destruction error if needed
+  //   }
+  //   res.redirect('/login'); // Redirect to login page after successful logout
+  // });
+
+  // Redirect to login page after clearing the cookie
+  res.redirect("auth/login");
 });
 
 router.get("/2FA", function (req, res, next) {
@@ -172,9 +304,10 @@ router.get("/lock-screen", function (req, res, next) {
 
 /* Admin*/
 
-// const people = ["geddy", "neil", "alex"];
-router.get("/dashboard", function (req, res, next) {
-  res.render("admin/dashboard", { title: "login" });
+// Dashboard route
+router.get("/dashboard", verifyToken, function (req, res) {
+  // Access user information from req.user and pass it to the view
+  const { username, email } = req.user;
+  res.render("admin/dashboard", { title: "Dashboard", username, email });
 });
-
 export default router;
